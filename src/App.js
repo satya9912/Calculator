cf8e2b3d1a87b68c2ed9be4ed90d0828e34d6519
satya9912 <satyakarri22@gmail.com>
2023-06-28 23:20:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Keypad from "./keypad"
+
+import './App.css'
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+      let [input, setInput] = useState("")
+
+      // Concatenating the clicked value to the current input value
+      
+      function handleClick(value) {
+
+            setInput(input + value)
+
+      }
+
+      // Evaluating the expression stored in the input value
+
+      function calculate(value) {
+
+            let outputVal = eval(input)
+            setInput(outputVal)
+
+      }
+
+      // Clearing the input value
+
+      function handleClear() {
+
+            setInput("")
+
+      }
+
+      return (
+            <div className="container">
+                  <h1>Calculator App using react</h1>
+                  <div className="caculator">
+                        <input type="text" value={input} className="output" readOnly />
+                        <Keypad handleClick={handleClick} handleClear={handleClear} calculate={calculate}></Keypad>
+
+                  </div>
+
+            </div>
+      )
 }
 
-export default App;
+export default App
